268c451a980a1bc05c48a31937ac331977b63d94
khanh2142 <k2142a@gmail.com>
2022-06-05 10:52:30 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const express = require("express");
 const route = express.Router();
 
 route.use("/student", require("../routes/student/index"));
+route.get("/", (req, res) => {
+  res.redirect("/student");
+});
 
 module.exports = route;
